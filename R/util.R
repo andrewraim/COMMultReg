3221ca92ad384a3977fc9d5942815595936ceca2
Andrew Raim <andrew.raim@gmail.com>
2020-06-07 14:22:30 -0400

    Cleaning out some unused functions

--- R/util.R
+++ R/util.R
@@ -24,33 +24,3 @@ inv_mlogit = function(x, base = 1) {
 mlogit = function(p, base = 1) {
 	log(p[-base] / p[base])
 }
-
-#' @export
-mult2cat = function(Y) {
-	stopifnot(all(sort(unique(as.numeric(Y))) == c(0,1)))
-	stopifnot(all(rowSums(Y) == 1))
-
-	n = nrow(Y)
-	p = ncol(Y)
-	z = integer(n)
-	for (j in 1:p) {
-		idx = which(Y[,j] == 1)
-		z[idx] = j
-	}
-	return(z)
-}
-
-#' @export
-cat2mult = function(Y, levels) {
-	n = length(Y)
-	k = length(levels)
-	res = matrix(0, n, k)
-	for (j in 1:k) {
-		idx = which(Y == levels[j])
-		res[idx,j] = 1
-	}
-	
-	colnames(res) = levels
-	return(res)
-}
-
