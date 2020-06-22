ca9656378c34eeb56558933dbee14ebd26817d07
Andrew Raim <andrew.raim@gmail.com>
2020-06-22 08:33:31 -0400

    Fixed internal calls to d_cmm_sample

--- R/cmm.R
+++ R/cmm.R
@@ -95,11 +95,10 @@ e_cmm = function(m, p, nu)
 
 	k = length(p)
 	xx = t(xsimplex(k, m))
-	p_mat = matrix(p, nrow(xx), k, byrow = TRUE)
+	P_mat = matrix(p, nrow(xx), k, byrow = TRUE)
 	nu_mat = rep(nu, nrow(xx))
-	m_mat = rep(m, nrow(xx))
 
-	f_all_unnorm = d_cmm_sample(xx, m_mat, p_mat, nu_mat, take_log = FALSE, normalize = FALSE)
+	f_all_unnorm = d_cmm_sample(xx, P_mat, nu_mat, take_log = FALSE, normalize = FALSE)
 	f_all = normalize(f_all_unnorm)
 	as.numeric(t(xx) %*% f_all)
 }
@@ -116,11 +115,10 @@ v_cmm = function(m, p, nu)
 
 	k = length(p)
 	xx = t(xsimplex(k, m))
-	p_mat = matrix(p, nrow(xx), k, byrow = TRUE)
+	P_mat = matrix(p, nrow(xx), k, byrow = TRUE)
 	nu_mat = rep(nu, nrow(xx))
-	m_mat = rep(m, nrow(xx))
 
-	f_all_unnorm = d_cmm_sample(xx, m_mat, p_mat, nu_mat, take_log = FALSE, normalize = FALSE)
+	f_all_unnorm = d_cmm_sample(xx, P_mat, nu_mat, take_log = FALSE, normalize = FALSE)
 	f_all = as.numeric(normalize(f_all_unnorm))
 	e = as.numeric(t(xx) %*% f_all)
 	t(xx) %*% (f_all * xx) - e %*% t(e)
